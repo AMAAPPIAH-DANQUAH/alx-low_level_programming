92bbde18a2360da7d13d3de577fce500331b5335
Ama Appiah-Danquah <aduakoamaa@gmail.com>
2023-07-21 07:23:04 -0700

    Isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/** file - main.h
+ * Description: Header contains all prototypes functions.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+*/
+
+int _isupper(int c);
+int _putchar(char c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+
+#endif
